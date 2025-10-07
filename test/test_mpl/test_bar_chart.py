19a2921ef8dfe9c768dcde3733a154e47e2eb8ea
Abhishek Kumar <abhishek.nitdelhi@gmail.com>
2025-10-08 00:30:14 +0530

    Fixed testing issues.

--- test/test_mpl/test_bar_chart.py
+++ test/test_mpl/test_bar_chart.py
@@ -14,10 +14,12 @@ def test_bar_chart_vertical_and_horizontal():
         values, labels=labels, orientation="vertical", title="v", dpi=100
     )
     assert isinstance(img_v, FastImage)
+    assert img_v.data is not None
     assert img_v.data[:8] == b"\x89PNG\r\n\x1a\n"
 
     img_h = plot_barchart(
         values, labels=labels, orientation="horizontal", title="h", dpi=100
     )
     assert isinstance(img_h, FastImage)
+    assert img_h.data is not None
     assert img_h.data[:8] == b"\x89PNG\r\n\x1a\n"
--- test/test_mpl/test_scatter_chart.py
+++ test/test_mpl/test_scatter_chart.py
@@ -7,8 +7,9 @@ from fmcp.mpl_mcp.core.scatter_chart import plot_scatter
 
 
 def test_scatter_basic_png():
-    x = [0, 1, 2]
-    y = [0, 1, 4]
+    x = [0.0, 1.0, 2.0]
+    y = [0.0, 1.0, 4.0]
     img = plot_scatter(x, y, title="s", figsize=[4, 3])
     assert isinstance(img, FastImage)
+    assert img.data is not None
     assert img.data[:8] == b"\x89PNG\r\n\x1a\n"
